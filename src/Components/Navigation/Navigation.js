eaf5015ce5e10880193d8462be0d9a9d0a47ac80
JaznanOfficial <jaznanofficial@gmail.com>
2021-10-18 17:04:15 +0600

    some added

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -6,15 +6,16 @@ import './Navigation.css';
 const Navigation = () => {
     return (
         <div>
-        <Navbar bg="light" expand="lg" fixed="top">
+        <Navbar bg="dark" expand="lg" fixed="top">
         <Container>
           <Navbar.Brand href="#home"><img src={logo} alt="img not found" width='150px' /></Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ms-auto">
-              <Nav.Link href="#home" className='fw-bolder fs-4 mx-3 px-0 text-primary nav-link-text'>Home</Nav.Link>
-              <Nav.Link href="#link" className='fw-bolder fs-4 mx-3 px-0 text-primary nav-link-text'>Link</Nav.Link>
-              <Nav.Link href="#link" className='fw-bolder fs-4 mx-3 px-0 text-primary nav-link-text'>Link</Nav.Link>
+            <Nav className="ms-auto nav-bar-custom">
+              <Nav.Link href="#home" className='fw-bolder fs-4 mx-3 px-0 text-light nav-link-text'>Home</Nav.Link>
+              <Nav.Link href="#link" className='fw-bolder fs-4 mx-3 px-0 text-light nav-link-text'>Discount</Nav.Link>
+              <Nav.Link href="#link" className='fw-bolder fs-4 mx-3 px-0 text-light nav-link-text'>Private Doctor</Nav.Link>
+              <Nav.Link href="#link" className='fw-bolder fs-4 mx-3 px-0 text-light nav-link-text'>Log In</Nav.Link>
               
             </Nav>
           </Navbar.Collapse>
