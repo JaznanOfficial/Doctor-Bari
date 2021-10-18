2054e6b46ff9314ace15bf9245a35022426cb1fa
JaznanOfficial <jaznanofficial@gmail.com>
2021-10-18 12:53:26 +0600

    added something

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from '../Header/Header';
 import Navigation from '../Navigation/Navigation';
+import Services from '../Services/Services';
 
 const Home = () => {
     return (
         <div>
             <Navigation></Navigation>
             <Header></Header>
+            <Services></Services>
         </div>
     );
 };
---
+++ src/Components/Service/Service.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Service = (props) => {
+    const { name, img, fee, details, specialist, experiences } = props.service;
+    console.log(props);
+    return (
+        <div></div>
+    );
+};
+
+export default Service;
